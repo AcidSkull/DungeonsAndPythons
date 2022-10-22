e01cabd828842a73e911eafd479ae0d270f593b6
AcidSkull <zielonyfantom@interia.pl>
2022-10-22 23:00:15 +0200

    Added idle animation

--- Characters/Player/Player.gd
+++ Characters/Player/Player.gd
@@ -1,14 +1,19 @@
 extends Character
 
 var direction = Vector2.ZERO
+onready var Game = get_parent()
 
-func _process(delta):
-	direction = get_direction()
-	if direction == Vector2.ZERO:
-		return
-	
+signal move(direction)
 
-func get_direction():
+func _ready():
+	var tmp = connect("move", Game, "_on_player_move")
+
+func _process(delta: float) -> void:
+	_get_direction()
+
+
+func _get_direction() -> void:
+	print(1)
 	if Input.is_action_pressed("ui_up"):
 		direction.y = -1
 	elif Input.is_action_pressed("ui_down"):
@@ -17,3 +22,7 @@ func get_direction():
 		direction.x = 1
 	elif Input.is_action_pressed("ui_left"):
 		direction.x = -1
+		
+	if direction != Vector2.ZERO:
+		emit_signal("move", direction)
+	direction = Vector2.ZERO
--- Game.gd
+++ Game.gd
@@ -1,3 +1,6 @@
 extends Node2D
 
+onready var player = get_node("Player")
 
+func _on_player_move(direction: Vector2):
+	player.global_position += (direction * 32)
