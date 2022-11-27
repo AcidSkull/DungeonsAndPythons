0cc546b48cecef852a920cde3656bad6f3c3ab30
AcidSkull <zielonyfantom@interia.pl>
2022-11-27 20:19:00 +0100

    Added tiles

--- Map.py
+++ Map.py
@@ -1,23 +1,30 @@
-import numpy as np
-from Tiles import Tile
+from Tiles import *
 import pygame
 
 class Map:
     def __init__(self, width: int, height: int):
-        self.width = width
-        self.height = height
-        self.tiles = self.init_tiles()
-        self.tile_png = pygame.image.load(".\\Assets\\Tiles\\Tile.png")
-        print(dir(self.tile_png))
+        self.width = width // 64
+        self.height = height // 64
+        # self.tiles = self.init_tiles()
+        self.tile_png = self.init_images()
     
     def init_tiles(self):
-        self.tiles = [[Tile(False) for y in range(self.height)] for x in range(self.width)]
-        return Tile
+        tiles = [[Tile() for y in range(self.height)] for x in range(self.width)]
+        return tiles
+    
+    def init_images(self):
+        tile_png = {
+            "Floor" : pygame.image.load(".\\Assets\\Tiles\\Tile.png"),
+            "Wall" : pygame.image.load(".\\Assets\\Tiles\\Wall.png"),
+        }
+        return tile_png
     
     def in_bound(self, x: int, y: int) -> bool:
         return 0 <= x < self.width and 0 <= y <self.height
     
     def render(self, screen: pygame.Surface):
-        for x in range(0, self.width, 64):
-            for y in range(0, self.height, 64):
-                screen.blit(self.tile_png, (x, y))
+        for x in range(0, self.width):
+            for y in range(0, self.height):
+                # print(self.tiles[x][y].get_texture())
+                screen.blit(self.tile_png["Floor"], (x*64, y*64))
+                # screen.blit(self.tiles[x][y].get_texture(), (x * 64, y * 64))
--- Tiles.py
+++ Tiles.py
@@ -1,8 +1,12 @@
 
 
 class Tile:
-    def __init__(self, blocked: bool):
+    def __init__(self, blocked: bool, texture):
         self.blocked = blocked
+        self.texture = texture
+    
+    def get_texture(self) -> str: return self.texture
 
-# class Floor(Tile):
-    # def __init__(self, texture: str):
+class Floor(Tile):
+    def __init__(self, texture):
+        super().__init__(False, texture)
